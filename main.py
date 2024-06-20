32a1d51661fc1cce7d8fb9b0b9f269275300a8cb
KIliev <kriskokz@gmail.com>
2024-06-20 17:33:37 +0300

    Task 3 started

--- main.py
+++ main.py
@@ -1,88 +1,24 @@
-### Task 2 ###
-import random
-import sys
+class Person:
+
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
 
 
-# Guess the number
+class PersonManager:
 
+    def __init__(self, people_list):
+        self.people_list = list(people_list)
 
-def random_number():
-    return random.randint(1, 10)
+    def add_person(self, people_list):
+        pass
 
+    def remove_person(self, people_list):
+    # Must be able to remove a person by name or surname
+        pass
 
-def player_name():
-    while True:
-        name = str(input("Enter your name: "))
-        if name != '':
-            return name
-        print("Name can't be empty.")
-        print()
-
-
-def player_guess():
-    lower_range = 1
-    upper_range = 10
-    while True:
-        try:
-            guess = int(input(f"Your guess (from {lower_range} to {upper_range}): "))
-            if guess == '':
-                print("This field cannot be empty!")
-                continue
-            return guess
-        except ValueError:
-            print(f"Please enter an integer from {lower_range} to {upper_range}!")
-            print('\n')
-            continue
-
-
-def guess_check(player_num: int, actual_num: int):
-    if player_num == actual_num:
-        return True
-    else:
-        return False
-
-
-def lower_or_higher(player_num: int, actual_num: int):
-    if player_num < actual_num:
-        return "Go higher!"
-    elif player_num > actual_num:
-        return "Go lower!"
-
-def continue_playing():
-    while True:
-        game_on = input("Would you like to play again? (Y/N)")
-        if game_on.lower().strip() == 'y':
-            print('\n' * 50)
-            return True
-        elif game_on.lower().strip() == 'n':
-            sys.exit()
-        else:
-            print("Invalid input. Please select Y or N.")
-            continue
-
-
-### Main logic of the game: ###
-
-# Initializing the attempts counter:
-while True:
-    count = 1
-
-    # Generating a random number to guess:
-    rand_num = random_number()
-    while True:
-
-        # Player choice is entered here:
-        pl_choice = player_guess()
-
-        # Numbers are checked:
-        check_game = guess_check(pl_choice, rand_num)
-        if check_game:
-            print(f"Congratulations! You won. It took you {count} tries.")
-            break
-        # If they're not equal, player gets feedback
-        else:
-            count += 1
-            print(lower_or_higher(pl_choice, rand_num))
-
-    # Continue playing prompt:
-    continue_playing()
+    def __str__(self):
+        for person in self.people_list:
+            print()
+    # TO continue implementation on how to print the list
---
+++ task2_exam.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python312
+
+
+### Task 2 ###
+import random
+import sys
+
+
+# Guess the number
+
+
+def random_number():
+    return random.randint(1, 10)
+
+
+def player_name():
+    while True:
+        name = str(input("Enter your name: "))
+        if name != '':
+            return name
+        print("Name can't be empty.")
+        print()
+
+
+def player_guess():
+    lower_range = 1
+    upper_range = 10
+    while True:
+        try:
+            guess = int(input(f"Your guess (from {lower_range} to {upper_range}): "))
+            if guess == '':
+                print("This field cannot be empty!")
+                continue
+            return guess
+        except ValueError:
+            print(f"Please enter an integer from {lower_range} to {upper_range}!")
+            print('\n')
+            continue
+
+
+def guess_check(player_num: int, actual_num: int):
+    if player_num == actual_num:
+        return True
+    else:
+        return False
+
+
+def lower_or_higher(player_num: int, actual_num: int):
+    if player_num < actual_num:
+        return "Go higher!"
+    elif player_num > actual_num:
+        return "Go lower!"
+
+def continue_playing():
+    while True:
+        game_on = input("Would you like to play again? (Y/N)")
+        if game_on.lower().strip() == 'y':
+            print('\n' * 50)
+            return True
+        elif game_on.lower().strip() == 'n':
+            sys.exit()
+        else:
+            print("Invalid input. Please select Y or N.")
+            continue
+
+
+### Main logic of the game: ###
+
+# Initializing the attempts counter:
+while True:
+    count = 1
+
+    # Generating a random number to guess:
+    rand_num = random_number()
+    while True:
+
+        # Player choice is entered here:
+        pl_choice = player_guess()
+
+        # Numbers are checked:
+        check_game = guess_check(pl_choice, rand_num)
+        if check_game:
+            print(f"Congratulations! You won. It took you {count} tries.")
+            break
+        # If they're not equal, player gets feedback
+        else:
+            count += 1
+            print(lower_or_higher(pl_choice, rand_num))
+
+    # Continue playing prompt:
+    continue_playing()
+
+
