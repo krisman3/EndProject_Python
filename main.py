a0e4f222f3a23267939edad64b02540beeb6b458
KIliev <kriskokz@gmail.com>
2024-06-20 15:25:26 +0300

    player_guess finished and tested

--- main.py
+++ main.py
@@ -12,26 +12,30 @@ def random_number():
 
 
 def player_name():
-    incorrect = True
-    while incorrect:
+    while True:
         name = str(input("Enter your name: "))
         if name != '':
-            incorrect = False
             return name
         print("Name can't be empty.")
         print()
 
 
-def player_guess(actual_num):
+def player_guess():
     global lower_range, upper_range
     while True:
         try:
             guess = input("Your guess: ")
+            if guess == '':
+                print("This field cannot be empty!")
+                continue
+            elif not guess.isdigit():
+                print(f"The guess must be an integer from {lower_range} to {upper_range}!")
+                continue
             if guess.isdigit():
                 if lower_range <= int(guess) <= upper_range:
                     return guess
         except ValueError:
-            print(
-                f"Please enter an integer from {lower_range} to {upper_range}!")  # Hard-coded the values for easier implementation
+            # Hard-coded the values for easier implementation
+            print(f"Please enter an integer from {lower_range} to {upper_range}!")
 
 
